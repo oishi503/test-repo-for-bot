5a345f335b49730a24e566689c70fcee2584500d
Oishi Bhattacharya <oishibhattacharya04@gmail.com>
2026-06-17 17:25:21 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,12 @@
+password = "secret123"
+api_key = "sk-abc123xyz"
+
+def divide(a, b):
+    return a / b
+
+def get_item(my_list, index):
+    return my_list[index]
+
+def login(user, pwd):
+    if pwd == password:
+        return True
